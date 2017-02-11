3ad822ffcd627c656ef17d8df82abefb70d2c292
Joaquin Maguire <thejmags@gmail.com>
2017-02-11 12:21:37 -0800

    Use WP shorthand for script URLs.

--- cuyfj/header.php
+++ cuyfj/header.php
@@ -1,17 +1,17 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html>
 <head>
     <title><?php echo get_bloginfo("name"); ?></title>
-    <!-- <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo("stylesheet_url"); ?>"> -->
-    <link rel="stylesheet/less" type="text/css" href="http://wplocal.net/wordpress/wp-content/themes/cuyfj/style.less" />
-    <link rel="stylesheet" type="text/css" href="http://wplocal.net/wordpress/wp-content/themes/cuyfj/scripts/jquery.mCustomScrollbar.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo("stylesheet_url"); ?>">
+    <link rel="stylesheet/less" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.less" />
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/scripts/jquery.mCustomScrollbar.css">
     <!-- Ye Olde Query of the Jaye -->
-    <script src="http://wplocal.net/wordpress/wp-content/themes/cuyfj/scripts/jquery-3.1.1.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/scripts/jquery-3.1.1.js"></script>
     <!-- Jquery plugin for non-hideous scrollbars! -->
-    <script src="http://wplocal.net/wordpress/wp-content/themes/cuyfj/scripts/jquery.mCustomScrollbar.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/scripts/jquery.mCustomScrollbar.js"></script>
     <!-- Client-side Less. -->
-    <script src="http://wplocal.net/wordpress/wp-content/themes/cuyfj/scripts/less.js"></script><!-- Nav pane management. -->
-    <script src="http://wplocal.net/wordpress/wp-content/themes/cuyfj/scripts/morelikehidebar.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/scripts/less.js"></script><!-- Nav pane management. -->
+    <script src="<?php echo get_template_directory_uri(); ?>/scripts/morelikehidebar.js"></script>
     <?php wp_head(); ?>
 </head>
 
